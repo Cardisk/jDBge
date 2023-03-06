26592d5f5a93d466b5b1419d4fab3bf42e81e577
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2023-03-06 19:56:00 +0100

    Keywords.h added made also some small changes inside operator<< for Token class

---
+++ components/Keywords.h
@@ -0,0 +1,21 @@
+//
+// Created by Matteo Cardinaletti on 06/03/23.
+//
+
+#ifndef JDBGE_KEYWORDS_H
+#define JDBGE_KEYWORDS_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> keywords {
+    "table",
+    "db",
+    "select",
+    "filter",
+    "insert",
+    "remove",
+    "delete",
+};
+
+#endif //JDBGE_KEYWORDS_H
